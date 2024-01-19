923680713c13202650b0447398c3e3a6b481af6b
Werner Heisenberg <47422999+YHJYH@users.noreply.github.com>
2024-01-19 17:06:03 +0000

    Update script.js

--- _layouts/script.js
+++ _layouts/script.js
@@ -5,7 +5,7 @@ function trackParcel() {
     if (trackingNumber) {
       // 如果追踪号码等于 "123456"，则跳转到特定的 HTML 页面
       if (trackingNumber === "123456") {
-        window.location.href = 'specific-details.html';
+        window.location.href = 'https://yhjyh.github.io/ParcelTracking/trackings/jan/123456.html';
       } else {
         // 否则，跳转到默认的页面，并传递追踪号码作为参数
         window.location.href = 'tracking-details.html?trackingNumber=' + encodeURIComponent(trackingNumber);
