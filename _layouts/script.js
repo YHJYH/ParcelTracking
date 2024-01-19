41d1c0e7f26626231b772c67c47469f3451251d4
Werner Heisenberg <47422999+YHJYH@users.noreply.github.com>
2024-01-19 17:03:28 +0000

    Update script.js

--- _layouts/script.js
+++ _layouts/script.js
@@ -1,10 +1,15 @@
 // script.js
 
-function redirectToDetails() {
+function trackParcel() {
     var trackingNumber = document.getElementById('tracking-number').value;
     if (trackingNumber) {
-      // 使用 window.location.href 跳转到新页面，并传递追踪号码作为参数
-      window.location.href = 'tracking-details.html?trackingNumber=' + encodeURIComponent(trackingNumber);
+      // 如果追踪号码等于 "123456"，则跳转到特定的 HTML 页面
+      if (trackingNumber === "123456") {
+        window.location.href = 'specific-details.html';
+      } else {
+        // 否则，跳转到默认的页面，并传递追踪号码作为参数
+        window.location.href = 'tracking-details.html?trackingNumber=' + encodeURIComponent(trackingNumber);
+      }
     } else {
       alert('Please enter a tracking number.');
     }
