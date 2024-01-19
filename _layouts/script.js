8ab454b4825262087b007abf167ed6be00362509
Werner Heisenberg <47422999+YHJYH@users.noreply.github.com>
2024-01-19 16:55:29 +0000

    update

--- _layouts/script.js
+++ _layouts/script.js
@@ -1,29 +1,12 @@
-function trackParcel() {
+// script.js
+
+function redirectToDetails() {
     var trackingNumber = document.getElementById('tracking-number').value;
-  
-    // Fetch tracking information from the JSON file
-    fetch('trackingData.json')
-      .then(response => response.json())
-      .then(data => {
-        var trackingData = data[trackingNumber];
-        if (trackingData) {
-          displayTrackingResult(trackingData);
-        } else {
-          alert('Tracking data not found for the provided number.');
-        }
-      })
-      .catch(error => console.error('Error:', error));
-  }
-  
-  function displayTrackingResult(data) {
-    var resultDiv = document.getElementById('tracking-result');
-    resultDiv.innerHTML = ''; // Clear previous content
-  
-    for (var i = data.length - 1; i >= 0; i--) {
-      var trackingInfo = data[i];
-      var timeStatusDiv = document.createElement('div');
-      timeStatusDiv.innerHTML = '<strong>Time:</strong> ' + trackingInfo.time + ', <strong>Status:</strong> ' + trackingInfo.status;
-      resultDiv.appendChild(timeStatusDiv);
+    if (trackingNumber) {
+      // 使用 window.location.href 跳转到新页面，并传递追踪号码作为参数
+      window.location.href = 'tracking-details.html?trackingNumber=' + encodeURIComponent(trackingNumber);
+    } else {
+      alert('Please enter a tracking number.');
     }
   }
   
